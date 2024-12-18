ca3bf91c76a9f9f891fbd49e14a8637e651b845c
debugVoyager <bintangwibawaput@gmail.com>
2024-12-18 16:50:56 +0800

    Implement a random RGB value on mouse hover
    Change the color of each square to a random RGB value
    on each mouse hover event.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const body = document.body
 const divContainer = document.createElement('div')
 const button = document.createElement('button')
 const footer = document.createElement('footer')
-const my_link = document.createElement('a')
+const myLink = document.createElement('a')
 
 let gridCountPerSide = 16
 let gridCountTotal = 256
@@ -11,11 +11,15 @@ body.append(button, divContainer, footer)
 divContainer.setAttribute('class', 'grid-container')
 button.setAttribute('class', 'btn')
 button.textContent = 'Set grid count'
-my_link.textContent = 'github.com/debugvoyager'
-my_link.setAttribute('target', '_blank')
-my_link.setAttribute('href', 'https://www.github.com/debugvoyager')
-my_link.setAttribute('rel', 'noopener noreferrer')
-footer.append(my_link)
+myLink.textContent = 'github.com/debugvoyager'
+myLink.setAttribute('target', '_blank')
+myLink.setAttribute('href', 'https://www.github.com/debugvoyager')
+myLink.setAttribute('rel', 'noopener noreferrer')
+footer.append(myLink)
+
+function getRandomRGB() {
+    return Math.floor(Math.random() * 256)
+}
 
 function renderGrid(gridCountTotal) {
     while (divContainer.firstChild) {
@@ -28,7 +32,7 @@ function renderGrid(gridCountTotal) {
 
     for (let child of divContainer.children) {
         child.setAttribute('class', 'grid')
-        child.addEventListener('mouseover', (e) => child.style.backgroundColor = '#333333')
+        child.addEventListener('mouseover', (e) => child.style.backgroundColor = `rgb(${getRandomRGB()}, ${getRandomRGB()}, ${getRandomRGB()})`)
     }
 }
 
